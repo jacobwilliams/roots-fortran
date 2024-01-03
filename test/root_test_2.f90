96ac5336af0a41ea14b7424a7d2bce67e657f096
Jacob Williams <jacobwilliams@users.noreply.github.com>
2024-01-02 23:02:13 -0600

    added a new test

---
+++ test/root_test_2.f90
@@ -0,0 +1,30 @@
+program root_test_2
+
+    use root_module, wp => root_module_rk
+
+    implicit none
+    integer,parameter :: n = 100000
+    real(wp) :: xzero
+    integer :: k, iflag
+    real(wp),dimension(n) :: p, out
+    type(brenth_solver) :: solver
+
+    call random_number(p); p = 1.5_wp * p
+    call solver%initialize(f, rtol=epsilon(1.0_wp), atol=epsilon(1.0_wp))
+    do k = 1, n
+       call solver%solve(0.0_wp,2.0_wp,xzero,out(k),iflag)
+    end do
+    print*, norm2(out)
+    print*, 'maxval =',maxval(abs(out))
+    print*, 'minval =',minval(abs(out))
+
+contains
+
+    function f(me,x)
+        class(root_solver),intent(inout) :: me
+        real(wp),intent(in) :: x
+        real(wp) :: f
+        f = x * sin(x) - p(k)
+    end function f
+
+end program root_test_2
